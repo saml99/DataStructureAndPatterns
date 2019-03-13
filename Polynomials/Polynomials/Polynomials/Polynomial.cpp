42bf7615912ea399baa008d4a539fb9b7793508f
Sam Lewis <samuel.lewis.com>
2019-03-13 22:21:30 +1100

    Added read input

--- Polynomials/Polynomials/Polynomials/Polynomial.cpp
+++ Polynomials/Polynomials/Polynomials/Polynomial.cpp
@@ -18,6 +18,22 @@ Polynomial Polynomial::operator*(const Polynomial& aRight) const
 // input operator for polynomials
 std::istream& operator>>(std::istream& aIStream, Polynomial& aObject)
 {
+	char line[256];
+	char *ptr;
+
+	aIStream.getline(line, 256);
+	sscanf(line, "%d", &aObject.fDegree);
+
+	aIStream.getline(line, 256);
+	ptr = strtok(line, " ");
+	int counter = 0;
+	while (ptr != NULL && counter < MAX_DEGREE+1)
+	{
+		sscanf(ptr, "%lf", &aObject.fCoeffs[counter]);
+		counter++;
+		ptr = strtok(NULL, " ");
+	}
+
 	return aIStream;
 }
 
